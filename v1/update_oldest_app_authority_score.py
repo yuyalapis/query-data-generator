806756234b64817f61acc8c1d34ecc50e8f2ee97
Yuya Sato <yuyasatowork@gmail.com>
2025-05-06 11:27:25 +0900

    Update update_oldest_app_authority_score.py debugging

--- v1/update_oldest_app_authority_score.py
+++ v1/update_oldest_app_authority_score.py
@@ -76,11 +76,14 @@ def execute_modify(sql: str):
 def save_oldest_app_authority_score():
   url = execute_select_one("select url from url_app_appl order by updated_at asc limit 1")
   current_date = datetime.datetime.now().date()
+  print(current_date)
   app_score_log = execute_select_one(f"select * from app_authority_score_logs where saved_at = '{current_date}' and url = '{url}'")
   if not app_score_log:
+    print("generating app_authority_score_logs")
     top_app_score_dict = execute_select_one("select url_to, count(url_from) as score from url_app_appl_suggested group by url_to order by score desc limit 1")
     target_app_score_dict = execute_select_one(f"select url_to, count(url_from) as score from url_app_appl_suggested group by url_to where url_to = '{url}'")
     target_app_score = target_app_score_dict["app_authority_score"] / top_app_score_dict["app_authority_score"]
+    print(target_app_score)
     execute_modify(f"insert into app_authority_score_logs(saved_at, url, app_authority_score) values ('{current_date}', '{url}' '{target_app_score}')")
     execute_modify(f"update url_app_appl set app_authority_score_saved_at = '{current_date}' where url = '{url}'")
 
